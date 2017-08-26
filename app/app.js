e47d0dc855e34be3e3c5cce34e5e14e141167b92
Mario <mario.saliba98@gmail.com>
2017-08-26 17:59:35 +0300

    builds a basic component

---
+++ app/app.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return <h1> Hello React, Im the new Component </h1>
+  }
+}
---
+++ app/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import App from './app.js';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
