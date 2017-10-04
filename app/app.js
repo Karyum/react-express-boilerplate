7d7298b68a024e2190581e731485174335495262
Mario <mario.saliba98@gmail.com>
2017-10-04 17:42:05 +0300

    move files '

--- app/app.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-
-export default class App extends Component {
-  render() {
-    return <h1 className="app"> Hello React, Im the new Component </h1>
-  }
-}
