e25fdeddb68405b407ba71ad6ee327d067144ac9
matthewdking <matthew.d.king@outlook.com>
2017-10-13 21:09:57 +0300

    adds rule to eslint relates #10

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const App = () => <h1 className="app"> Hey I&lsquo;m React&lsquo;s setup </h1>;
+const App = () => <h1 className="app"> Hey I'm React's setup </h1>;
 
 export default App;
